3a2aba83be0ebea2d3f211d64f2989c31d414026
TightenPreacher <731995484@qq.com>
2019-10-23 22:18:22 +0800

    init demo

---
+++ src/views/three/cameras/index.vue
@@ -0,0 +1,36 @@
+<template />
+
+<script>
+import DropdownMenu from '@/components/Share/DropdownMenu'
+
+export default {
+  name: 'Documentation',
+  components: { DropdownMenu },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+	.documentation-container {
+		margin: 50px;
+		display: flex;
+		flex-wrap: wrap;
+		.document-btn {
+			margin-left: 50px;
+			display: block;
+			cursor: pointer;
+			background: black;
+			color: white;
+			height: 60px;
+			width: 200px;
+			margin-bottom: 16px;
+			line-height: 60px;
+			font-size: 20px;
+			text-align: center;
+		}
+	}
+</style>
